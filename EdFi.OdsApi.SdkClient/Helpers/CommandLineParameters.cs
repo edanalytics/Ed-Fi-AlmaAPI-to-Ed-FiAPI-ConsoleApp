6208ab0cded14ded574c9824435e5efcbd942295
margarito <Margarito.jimenez@developers.net>
2022-06-13 17:01:19 -0500

    Adding awsSourceConnectionName and awsDestinationConnectionName parameters

--- EdFi.OdsApi.SdkClient/Helpers/CommandLineParameters.cs
+++ EdFi.OdsApi.SdkClient/Helpers/CommandLineParameters.cs
@@ -10,53 +10,66 @@ namespace EdFi.AlmaToEdFi.Cmd.Helpers
 {
    static class CommandLineParameters
     {
+        public static readonly string  _defaultValue = "";
         public static RootCommand GetCommandLineParameters()
         {
-            // Create some options: 
+           
+        // Create some options:
+        var awsSourceConnectionName = new Option<string>(
+             "--awsSourceConnectionName",
+             description: "Aws Parameters Store Could have multiple sources.", getDefaultValue: () => _defaultValue);
+            awsSourceConnectionName.AddValidator(r =>
+            {
+                var sourceConnectionName = "/AlmaApi/Connections/Alma/{SourceConnection1}/url \r /AlmaApi/Connections/Alma/{SourceConnection2}/url";
+                var value = r.GetValueOrDefault<string>();
+                if (!string.IsNullOrEmpty(value))
+                    if (value.ToLower().Contains("your_source_connection_parameters") || value.Contains("<") || value.Contains(">"))
+                        return $"{r.Token.Value} is not valid \r {sourceConnectionName}";
+                return null;
+            });
+            var awsDestinationConnectionName = new Option<string>(
+             "--awsDestinationConnectionName",
+             description: "Aws Parameters Store Could have multiple destinations.", getDefaultValue: () => _defaultValue);
+            awsDestinationConnectionName.AddValidator(r =>
+            {
+                var destinationConnectionName = "/AlmaApi/Connections/Edfi/{DestinationConnection1}/url \r /AlmaApi/Connections/Edfi/{DestinationConnection2}/url";
+
+                var value = r.GetValueOrDefault<string>();
+                if (!string.IsNullOrEmpty(value))
+                    if (value.ToLower().Contains("your_destination_connection_parameters") || value.Contains("<") || value.Contains(">"))
+                        return $"{r.Token.Value} is not valid \r {destinationConnectionName}";
+                return null;
+            });
+
             var schoolYearFilter = new Option<string>(
                 "--schoolYearFilter",
-                description: "if you want to filter by School Year , pass the value (e.g. 2019-2020)", getDefaultValue: () => "");
+                description: "if you want to filter by School Year , pass the value (e.g. 2019-2020)", getDefaultValue: () => _defaultValue);
                 schoolYearFilter.AddValidator(r =>
+                    {
+                        var pattern = @"^\d{4}(-\d{4})$";
+                        var value = r.GetValueOrDefault<string>();
+                        if (!string.IsNullOrEmpty(value))
+                            if (!Regex.IsMatch(value, pattern, RegexOptions.IgnoreCase))
+                                return $"{r.Token.Value} - should be something like  2020-2021";
+                        return null;
+                    });
+
+            var schoolFilter = new Option<string>(
+             "--schoolFilter",
+             description: "The School Information you want to request.", getDefaultValue: () => _defaultValue);
+            schoolFilter.AddValidator(r =>
                 {
-                    string pattern = @"^\d{4}(-\d{4})$";
                     var value = r.GetValueOrDefault<string>();
                     if (!string.IsNullOrEmpty(value))
-                        if (!Regex.IsMatch(value, pattern, RegexOptions.IgnoreCase))
-                            return $"{r.Token.Value} should be something like  2020-2021";
+                        if (value.ToLower().Contains("your_school") || value.Contains("<") || value.Contains(">"))
+                            return $"{r.Token.Value} is not valid";
                     return null;
                 });
-             
 
-            var awsKey = new Option<string>(
-                        "--awsKey",
-                        description: "Your Aws Key", getDefaultValue: () => "");
-                        awsKey.Required = false;
-                        awsKey.AddValidator(r =>
-                        {
-                            var value = r.GetValueOrDefault<string>();
-                            if (!string.IsNullOrEmpty(value))
-                                if (value.ToLower().Contains("your_aws_key") ||  value.Contains("<") ||  value.Contains(">"))
-                                    return $"{value} is not a valid Key";
-                            return null;
-                        });
-
-            var awsSecret = new Option<string>(
-                        "--awsSecret",
-                        description: "your Aws Secret", getDefaultValue: () => "");
-                        awsSecret.Required = false;
-                        awsSecret.AddValidator(r =>
-                        {
-                            var value = r.GetValueOrDefault<string>();
-                            if (!string.IsNullOrEmpty(value))
-                                if (value.ToLower().Contains("your_aws_secret") || value.Contains("<") || value.Contains(">"))
-                                    return $"{value} is not a valid Secret";
-                            return null;
-                        });
 
             var awsRegion = new Option<string>(
                 "--awsRegion",
                 description: "your Aws Region (e.g. us-east-1)", getDefaultValue: () => "");
-                awsRegion.Required = false;
                 awsRegion.AddValidator(r =>
                 {
                     var value = r.GetValueOrDefault<string>();
@@ -68,8 +81,7 @@ namespace EdFi.AlmaToEdFi.Cmd.Helpers
 
             var awsLoggingGroupName = new Option<string>(
                 "--awsLoggingGroupName",
-                description: "Each log stream has to belong to one log group (e.g. AlmaApi)", getDefaultValue: () => "");
-                awsLoggingGroupName.Required = false;
+                description: "Each log stream has to belong to one log group (e.g. AlmaApi)", getDefaultValue: () => _defaultValue);
                 awsLoggingGroupName.AddValidator(r =>
                     {
                         var value = r.GetValueOrDefault<string>();
@@ -82,36 +94,44 @@ namespace EdFi.AlmaToEdFi.Cmd.Helpers
             // Add the options to a root command:
             var rootCommand = new RootCommand
                 {
+                    awsSourceConnectionName,
+                    awsDestinationConnectionName,
                     schoolYearFilter,
-                    awsKey,
-                    awsSecret,
+                    schoolFilter,
                     awsRegion,
                     awsLoggingGroupName
                 };
             rootCommand.Description = "EdFi.AlmaToEdFi.Cmd (Example of parameters :  --schoolYearFilter 2019-2020,--awsRegion us-east-1,--awsLoggingGroupName AlmaApi)";
+            
             return rootCommand;
         }
 
-        public static AppSettings CheckParameters(AppSettings settings, string schoolYearFilter,
-                                                   string awsKey, string awsSecret, string awsRegion,
-                                                   string awsLoggingGroupName)
+        public static AppSettings CheckParameters(
+            AppSettings settings,
+            string awsSourceConnectionName,
+            string awsDestinationConnectionName,
+            string schoolYearFilter,string schoolFilter,
+            string awsRegion,string awsLoggingGroupName)
         {
             //ovewrite the appsetting value with the parameter
             settings.AwsConfiguration = new AwsConfiguration();
+            if (!string.IsNullOrEmpty(awsSourceConnectionName.Trim()))
+                settings.AlmaAPI.Connections.SourceConnectionFilter = awsSourceConnectionName;
+
+            if (!string.IsNullOrEmpty(awsDestinationConnectionName.Trim()))
+                settings.AlmaAPI.Connections.TargetConnectionFilter = awsDestinationConnectionName;
+
             if (!string.IsNullOrEmpty(schoolYearFilter.Trim()))
-                settings.SourceAlmaAPISettings.SchoolYearFilter = schoolYearFilter;
+                settings.AlmaAPI.Connections.Alma.SourceConnection.SchoolYearFilter = schoolYearFilter;
 
-            if (!string.IsNullOrEmpty(awsKey.Trim()))
-                settings.AwsConfiguration.AWSAccessKey = awsKey;
-
-            if (!string.IsNullOrEmpty(awsSecret.Trim()))
-                settings.AwsConfiguration.AWSSecretKey = awsSecret;
+            if (!string.IsNullOrEmpty(schoolFilter.Trim()))
+                settings.AlmaAPI.Connections.Alma.SourceConnection.SchoolFilter = schoolFilter;
 
             if (!string.IsNullOrEmpty(awsRegion.Trim()))
-                settings.AwsConfiguration.AWSRegion = awsRegion;
+                settings.Logging.Region = awsRegion;
 
             if (!string.IsNullOrEmpty(awsLoggingGroupName.Trim()))
-                settings.AwsConfiguration.AWSLoggingGroupName = awsLoggingGroupName;
+                settings.Logging.LogGroup = awsLoggingGroupName;
             return settings;
         }
     }
