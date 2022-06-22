969fabd05996a0ccd9eb9835500b338f82ba6b2a
margarito <Margarito.jimenez@developers.net>
2022-06-22 11:19:02 -0500

    Adding to the log the Attendance codes that Failed to mapping

--- EdFi.OdsApi.SdkClient/Services/Transform/Alma/StudentSchoolAttendanceTransformer.cs
+++ EdFi.OdsApi.SdkClient/Services/Transform/Alma/StudentSchoolAttendanceTransformer.cs
@@ -1,6 +1,7 @@
-﻿using Alma.Api.Sdk.Models;
+using Alma.Api.Sdk.Models;
 using EdFi.AlmaToEdFi.Cmd.Services.Transform.Descriptor;
 using EdFi.OdsApi.Sdk.Models.Resources;
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 
 namespace EdFi.AlmaToEdFi.Cmd.Services.Transform.Alma
@@ -13,12 +14,15 @@ namespace EdFi.AlmaToEdFi.Cmd.Services.Transform.Alma
     {
         private readonly ISessionNameTransformer _sessionNameTransformer;
         private readonly IDescriptorMappingService _descriptorMappingService;
+        private readonly ILogger _appLog;
         public StudentSchoolAttendanceTransformer(
+            ILoggerFactory logger,
             IDescriptorMappingService descriptorMappingService,
             ISessionNameTransformer sessionNameTransformer)
         {
             _descriptorMappingService = descriptorMappingService;
             _sessionNameTransformer = sessionNameTransformer;
+            _appLog = logger.CreateLogger("Student School Attendance Event Transformer");
         }
         public EdFiStudentSchoolAttendanceEvent TransformSrcToEdFi(int schoolId, Attendance srcAttendance, List<Session> almaSessions)
         {
@@ -33,12 +37,15 @@ namespace EdFi.AlmaToEdFi.Cmd.Services.Transform.Alma
         }
         public string GetEdFiAttendanceEventCategoryDescriptors(string srcreportedStatus, string srcStatusModifier)
         {
+            var attendanceCode = srcreportedStatus;
             if (srcreportedStatus == "Absent")
                 if (srcStatusModifier == "Excused" || srcStatusModifier == "Unexcused")
                     srcreportedStatus = $"{srcStatusModifier} {srcreportedStatus}";
             var map = _descriptorMappingService.MappAlmaToEdFiDescriptor("AttendanceEventCategoryDescriptor", srcreportedStatus);
             if (map == null)
-                map = _descriptorMappingService.MappAlmaToEdFiDescriptor("AttendanceEventCategoryDescriptor", srcreportedStatus);
+                map = _descriptorMappingService.MappAlmaToEdFiDescriptor("AttendanceEventCategoryDescriptor", attendanceCode);
+            if (map == null)
+                _appLog.LogInformation($"Failed to mapping ---> Reported Status: {attendanceCode} , StatusModifier: {srcStatusModifier}");
             return map;
         }
     }
