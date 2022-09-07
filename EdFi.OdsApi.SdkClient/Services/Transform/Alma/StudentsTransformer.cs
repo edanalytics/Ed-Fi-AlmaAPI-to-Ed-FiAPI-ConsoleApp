e11e71eb13f62ec37681ec59a0c7819383c85211
chafner-ea <chafner@edanalytics.org>
2022-09-07 15:55:44 -0400

    Added handling for a default value on student Dob returned from Alma that was inside of the SQL datetime window when Alma returned a student with a null or empty date of birth.

--- EdFi.OdsApi.SdkClient/Services/Transform/Alma/StudentsTransformer.cs
+++ EdFi.OdsApi.SdkClient/Services/Transform/Alma/StudentsTransformer.cs
@@ -1,4 +1,4 @@
-﻿using Alma.Api.Sdk.Models;
+using Alma.Api.Sdk.Models;
 using EdFi.AlmaToEdFi.Cmd.Services.Transform.Descriptor;
 using EdFi.OdsApi.Sdk.Models.Resources;
 using System;
@@ -20,6 +20,12 @@ namespace EdFi.AlmaToEdFi.Cmd.Services.Transform.Alma
         }
         public EdFiStudent TransformSrcToEdFi(Student srcStudent)
         {
+            //Check to see if the Student DoB is null or empty. The code below will enter "1/1/0001" as the default value even when fed a null value.
+            //This is an issue because SQL rejects anything before 01/01/1753.
+            if (String.IsNullOrEmpty(srcStudent.dob))
+            {
+                srcStudent.dob = "9999-12-30";
+            }
             return  new EdFiStudent(null, srcStudent.id, null, null, null, Convert.ToDateTime(srcStudent.dob), null, 
                                     GetEdFiGenderDescriptors(srcStudent.gender), null, null, null, srcStudent.firstName,
                                     null, null, srcStudent.lastName, null, srcStudent.middleName);
